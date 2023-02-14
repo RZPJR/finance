1ef579debf28149742babe90cc58471b8b747d58
junior <junior@edenfarm.id>
2023-02-14 16:38:15 +0700

    component

--- src/main.js
+++ src/main.js
@@ -11,14 +11,7 @@ import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
 import '@mdi/font/css/materialdesignicons.css'
 
-import { 
-  SelectGlossary,
-  DetailRowNew,
-  ConfirmationDialogNew,
-  SelectArea,
-  SelectWarehouse,
-  SelectMerchant,
-} from "@vue-mf/global";
+import { Components } from "@vue-mf/global";
 import Vuetify from "vuetify/lib";
 Vue.use(require('vue-moment'));
 
@@ -41,14 +34,7 @@ Vue.directive('privilege', {
 })
 
 Vue.mixin({
-  components: { 
-    SelectGlossary,
-    DetailRowNew,
-    ConfirmationDialogNew,
-    SelectArea,
-    SelectWarehouse,
-    SelectMerchant,
-  },
+  components: Components,
   data: function() {
     return {
       status: [
